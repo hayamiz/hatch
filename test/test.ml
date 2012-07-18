d53f721be2810481b812f147baf5d3f98aa1a7b1
Yuto Hayamizu <y.hayamizu@gmail.com>
2012-07-18 20:26:01 +0900

    fix test runner mechanism

--- test/test.ml
+++ test/test.ml
@@ -5,6 +5,29 @@ open ParserUtil
 
 let id x = x
 
+let suites: OUnit.test list ref = ref []
+let add_suites suite =
+  suites := suite :: !suites
+
+(* test of paserUtil.ml *)
+
+let test_string_of_token _ =
+  assert_equal ~printer:id "INT 1" (ParserUtil.string_of_token (INT 1));
+  assert_equal ~printer:id "PLUS" (ParserUtil.string_of_token PLUS);
+  assert_equal ~printer:id "MINUS" (ParserUtil.string_of_token MINUS)
+
+let test_string_of_tokens _ =
+  assert_equal ~printer:id "INT 1; PLUS; INT 2" (ParserUtil.string_of_tokens [INT 1; PLUS; INT 2])
+
+let _ = add_suites begin "ParserUtil" >::: [
+  "string_of_token" >:: test_string_of_token;
+  "string_of_tokens" >:: test_string_of_tokens
+] end
+
+
+
+(* test of lexer.ml *)
+
 let tokens_from_string str =
   let lexbuf = Lexing.from_string str
   and toks = ref [] in
@@ -26,24 +49,16 @@ let test_arith_exp _ =
   assert_equal [INT 1; PLUS; INT 2; MUL; INT 3] (tokens_from_string "1+2*3");
   assert_equal [LPAREN; INT 1; PLUS; INT 2; RPAREN; MUL; INT 3] (tokens_from_string "(1+2)*3")
 
-let test_string_of_token _ =
-  assert_equal ~printer:id "INT 1" (ParserUtil.string_of_token (INT 1));
-  assert_equal ~printer:id "PLUS" (ParserUtil.string_of_token PLUS);
-  assert_equal ~printer:id "MINUS" (ParserUtil.string_of_token MINUS)
-
-let test_string_of_tokens _ =
-  assert_equal "INT 1; PLUS; INT 2" (ParserUtil.string_of_tokens [INT 1; PLUS; INT 2])
-
-let suite = "Parser" >::: [
-  "string_of_token" >:: test_string_of_token;
-  "string_of_tokens" >:: test_string_of_tokens
-]
-
-let _ = run_test_tt_main suite
-
-let suite = "Lexer" >::: [
+let _ = add_suites begin "Lexer" >::: [
   "simple arithmetic exp" >:: test_simple_arith_exp;
   "arithmetic exp" >:: test_arith_exp;
-]
+] end
 
-let _ = run_test_tt_main suite
+
+
+let _ =
+  List.iter
+	(fun suite ->
+	   let _ = run_test_tt ~verbose:true suite
+	   in ())
+	!suites
