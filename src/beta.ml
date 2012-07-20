8094b4b135c118b71fee6ae89669635ce16c77c3
Yuto Hayamizu <y.hayamizu@gmail.com>
2012-07-21 02:50:50 +0900

    done with beta reduction

--- src/beta.ml
+++ src/beta.ml
@@ -1,3 +1,46 @@
 
+open Normal
+
+let rec do_beta_reduce env nexp =
+  let findsym env sym =
+	if Smap.mem sym env then
+	  Smap.find sym env
+	else
+	  sym
+  in
+  let addsym env from_sym to_sym =
+	let new_env = Smap.add from_sym to_sym env in
+	  new_env
+  in
+  match nexp with
+	  NexpVar id ->
+		NexpVar (findsym env id)
+	| NexpInt _
+	| NexpFloat _
+	| NexpString _
+	| NexpBool _
+	| NexpUndef ->
+		nexp
+	| NexpLambda (params, body) ->
+		NexpLambda (List.map (fun s -> findsym env s) params,
+					do_beta_reduce env body)
+	| NexpApply (f, args) ->
+		NexpApply (findsym env f,
+				   List.map (fun s -> findsym env s) args)
+	| NexpBind (id, v) ->
+		NexpBind (id, do_beta_reduce env v)
+	| NexpLet (id, NexpVar id', body) ->
+		let env' = (addsym env id id') in
+		  do_beta_reduce env' body
+	| NexpLet (id, v, body) ->
+		NexpLet (id, do_beta_reduce env v, do_beta_reduce env body)
+	| NexpPrefix (op, v) ->
+		NexpPrefix (op, findsym env v)
+	| NexpInfix (op, v1, v2) ->
+		NexpInfix (op, findsym env v1, findsym env v2)
+	| NexpIf (cond, if_body, else_body) ->
+		NexpIf (findsym env cond, do_beta_reduce env if_body, do_beta_reduce env else_body)
+
+
 let beta_reduce exp =
-  exp
+  do_beta_reduce Smap.empty exp
--- test/test.ml
+++ test/test.ml
@@ -1000,6 +1000,158 @@ let _ = add_suites begin "Alpha" >::: [
 
 
 
+(* test of beta.ml *)
+
+let assert_eq_beta expected actual =
+  assert_equal ~msg:("Beta-reducing: " ^ (string_of_normal_expr actual))
+	~cmp:(fun x y -> normal_expr_equal ~exact:true x y)
+	~printer:(fun ne -> "\n" ^ (string_of_normal_expr ne) ^ "\n")
+	expected actual
+
+let test_beta_reduce _ =
+  assert_eq_beta
+	  (NexpLet ("x",
+				NexpInt 1,
+				NexpVar "x"))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpVar "x"))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpLambda (["z"],
+						  NexpVar "x")))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpLambda (["z"],
+									  NexpVar "y")))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpApply ("x", ["z"])))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpApply ("y", ["z"])))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpApply ("z", ["x"])))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpApply ("z", ["x"])))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpBind ("y", NexpVar"z")))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpBind ("y", NexpVar"z")))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpBind ("z", NexpVar"x")))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpBind ("z", NexpVar"y")))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpLet ("z", NexpInt 1,
+								   NexpVar"x")))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpLet ("z", NexpInt 1,
+								   NexpVar"y")))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpPrefix (PrefixPlus, "x")))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpPrefix (PrefixPlus, "y")))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpInfix (InfixPlus, "x", "z")))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpInfix (InfixPlus, "y", "z")))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpIf ("z", NexpVar "v", NexpVar "w")))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpIf ("z", NexpVar "v", NexpVar "w")))))
+  ;
+
+  assert_eq_beta
+	(NexpLet ("x",
+			  NexpInt 1,
+			  NexpIf ("x", NexpVar "x", NexpVar "x")))
+	(Beta.beta_reduce
+	   (NexpLet ("x",
+				 NexpInt 1,
+				 NexpLet ("y",
+						  NexpVar "x",
+						  NexpIf ("y", NexpVar "y", NexpVar "y")))))
+  ;
+  ()
+
+let _ = add_suites begin "Beta" >::: [
+  "beta_reduce" >:: test_beta_reduce;
+] end
+
+
+
 
 let _ =
   List.iter
